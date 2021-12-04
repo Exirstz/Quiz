5fb9ced337c9b3132d236af444670487922c978c
Furkhon Nurdiyanto <55175431+Exirstz@users.noreply.github.com>
2021-12-04 07:02:57 +0700

    Update game.js

--- game.js
+++ game.js
@@ -73,7 +73,7 @@ getNewQuestion = () => {
     const questionIndex = Math.floor(Math.random() * availableQuesions.length);
     currentQuestion = availableQuesions[questionIndex];
     if (currentQuestion.question == "burung") {
-        question.innerHTML = '<img src="bird.png" alt="Image1" width="200" height="200">'
+        question.innerHTML = '<img src="https://i.pinimg.com/564x/7b/7d/e7/7b7de7c583100f9c8144da0a974f96d6.jpg" alt="Image1" width="200" height="200">'
     }
     else {
         question.innerHTML = currentQuestion.question;
